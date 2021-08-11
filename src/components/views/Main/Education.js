081116e65741b3806841568515102df58d2601ed
PavelBulaienko <57744770+PavelBulaienko@users.noreply.github.com>
2021-08-11 14:59:21 +0300

    Update edication

--- src/components/views/Main/Education.js
+++ src/components/views/Main/Education.js
@@ -5,7 +5,6 @@ const Education = () => (
       <li>
         <h3>Polytechnical Institute KPI</h3>
         <p>"Radio engineering faculty"</p>
-        <p>Higher education</p>
         <small className="small">September 2018 - June 2022</small>
       </li>
     </ul>
