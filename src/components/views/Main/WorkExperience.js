d0f3e7a1ac15d57a7a61f99d8172801822fdd277
Pavel Bulaienko <p.bulaienko@ioalabs.com>
2022-12-12 14:50:35 +0200

    feature: update Work experience section

--- src/components/views/Main/WorkExperience.js
+++ src/components/views/Main/WorkExperience.js
@@ -7,6 +7,26 @@ const WorkExperience = () => (
         <p>
           Company:
           <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://nimbusplatform.io/"
+          >
+            {" "}
+            Nimbus
+          </a>
+        </p>
+        <p>
+          DAO-governed platform
+          <br/>
+          React • HTML / CSS • JavaScript
+        </p>
+        <small className="small">September 2021 - December 2022 (1 year 4 month)</small>
+      </li>
+      <li>
+        <h3>Front-end Developer</h3>
+        <p>
+          Company:
+          <a
             rel="noreferrer"
             target="_blank"
             href="https://terrapromo.digital/"
@@ -19,7 +39,7 @@ const WorkExperience = () => (
           Website development on bootstrap • HTML / CSS • PHP • Figma •
           JavaScript
         </p>
-        <small className="small">March 2021 - June 2021</small>
+        <small className="small">March 2021 - June 2021 (4 month)</small>
       </li>
       <li>
         <h3>Customer support</h3>
